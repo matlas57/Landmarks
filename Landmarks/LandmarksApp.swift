ddb0cde24dbd4eb5ef0514eab633591c303400dd
Matan Atlas <matanatlas2002@gmail.com>
2025-12-02 14:06:27 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Matan Atlas on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
